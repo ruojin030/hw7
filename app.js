3377f8f03602167c3fef09b7fb993d15cd9ff4f4
张若瑾 <ruojin@zhangruojindeMacBook-Pro.local>
2019-04-10 18:24:45 -0400

    I hate cse308

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+const app = express()
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'cse356',
+    database:'hw7'
+})
+connection.connect()
+app.get("/hw7",function(req,res){
+    req.query.club
+    req.query.pos
+    var q = "SELECT Player,A WHERE Club=\""+req.query.club+"\" and POS=\""+req.query.pos+"\""
+    connection.query(q,function(err,rows,fields){
+        if (err) console.log(err);
+        var high = 0
+        var total = 0
+        var Player = ""
+        var count = 0
+        for(var i in rows){
+            total = total + rows[i].A
+            if(rows[i].A >high){
+                Player = rows[i].Player
+            }
+            count++
+        }
+        var avg = total/count
+        res.json({'club':req.quere.club,'pos':req.query.pos,'max_assists': high,'player':Player, 'avg_assists':avg})
+    })
+
+})
