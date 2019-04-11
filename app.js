cf721dd8efd1d76946d33d99ff606d1ea04b7250
张若瑾 <ruojin@zhangrujindeMBP.home>
2019-04-11 00:07:23 -0400

    >=

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get("/hw7",function(req,res){
         var count = 0
         for(var i in rows){
             total = total + rows[i].A
-            if(rows[i].A >high){
+            if(rows[i].A >=high){
                 Player = rows[i].Player
                 high = rows[i].A
             }
