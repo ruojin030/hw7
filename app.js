8eb7b86fdaf6d1b9e1b0609f61fc1456ebff22c0
张若瑾 <ruojin@zhangrujindeMBP.home>
2019-04-10 23:40:52 -0400

    mysql

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var mysql = require('mysql')
 
 var connection = mysql.createConnection({
     host: 'localhost',
-    user: 'username@localhost',
+    user: 'username',
     password:'password',
     database:'hw7'
 })
